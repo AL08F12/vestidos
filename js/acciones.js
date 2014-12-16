2a73b33a8ad134f9d04277dcf7005967d380945f
AL08F12 <ISABELLACOYOTL@hotmail.com>
2014-12-16 10:19:32 -0600

    uno

---
+++ js/acciones.js
@@ -0,0 +1,35 @@
+function buscarsirena()
+{
+ $.ajax({
+	type: "POST",
+	url: "http://192.168.1.166/proyecto/Cortesirena.php"
+	 
+ }).done(function(msg){
+	 var DatosJSON = JSON.parse(msg);
+	 
+	 if (DatosJSON.datos ==1)
+	 {
+		 $('#corte').empty();
+		  for (var i = 0; i <DatosJSON.vestidos.length  ;i++)
+		  {
+			  $('#corte').append(' <div align="center"> <hr color="#6600FF">     Color:    '+DatosJSON.vestidos[i].Color+'  <br>    <br>  Fotografia: <img src="http://192.168.1.166/proyecto/recursos/fotos/'+DatosJSON.vestidos[i].Modelo+'.jpg">    <br>    <br>    Talla:    '+DatosJSON.vestidos[i].Talla+'    <hr color="#6600FF">     </div>');
+		  }
+		 
+	 }
+	 
+if (DatosJSON.datos ==0)
+	 {
+		 alert ("no hay vestidos con ese corte")
+	 }
+ });
+}
+
+
+$(document).ready(function(e){
+	 document.addEventListener("deviceready",function(){
+		 $('#sirena').tap(function(){
+			 		 buscarsirena()
+			 });
+		 
+	 });
+});
